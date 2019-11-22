f67f1b4df770bd065cdcb559a25978743fa91fc9
harry334421 <58018547+harry334421@users.noreply.github.com>
2019-11-23 05:54:32 +0800

    Delete Test_3_1.py

--- Test_3_1.py
+++
@@ -1,8 +0,0 @@
-import gensim
-from gensim.models import word2vec
-
-if __name__=='__main__':
-    model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin',binary=True)
-    distance = model.distance('woman', 'man')
-    print(distance)
-    
